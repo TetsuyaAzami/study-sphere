2b69e84cbc7ff67ec32102747e9ca9e025b242aa
Tetsuya Azami <zqianjian35@gmail.com>
2023-05-06 19:03:50 +0900

    srcディレクトリ配下への移動と不要な記述の削除

---
+++ front/src/pages/index.tsx
@@ -0,0 +1,18 @@
+import Head from 'next/head'
+import styles from '@/styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Login</title>
+        <meta name="description" content="Login page for studySphere" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main>
+
+      </main>
+    </>
+  )
+}
