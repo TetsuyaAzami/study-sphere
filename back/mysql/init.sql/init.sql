5a062cc39d0b5c690cf47cdce90d86d2fb6a2327
Tetsuya Azami <zqianjian35@gmail.com>
2023-05-22 22:13:15 +0900

    DDLおよびDMLの修正

--- back/mysql/init.sql/init.sql
+++ back/mysql/init.sql/init.sql
@@ -1,20 +1,65 @@
 DROP SCHEMA IF EXISTS study_sphere;
+
 CREATE SCHEMA study_sphere;
+
 USE study_sphere;
 
-DROP TABLE IF EXISTS `users`;
+DROP TABLE IF EXISTS email_verifications;
 
-create table users (
-	id VARCHAR(8) PRIMARY KEY,
-	username VARCHAR(255),
-	password VARCHAR(255),
-	email VARCHAR(255),
-	age INTEGER,
-	enabled BOOLEAN,
-	roles VARCHAR(255)
+CREATE TABLE email_verifications(
+	email_verification_id INT AUTO_INCREMENT PRIMARY KEY,
+	uuid VARCHAR(100) NOT NULL,
+	email VARCHAR(254) NOT NULL,
+	expiration_date DATETIME NOT NULL,
+	created_at DATETIME NOT NULL,
+	updated_at DATETIME NOT NULL
 );
 
-INSERT INTO users (id, username, password, email, age, enabled, roles) VALUES
-    ('10000000', 'test1', '$2a$08$7563MWncbW/oenHWfZSKE.jWeMARqdO/WZajbstas2xUamOBXDWz6', 'test1@example.com', 30, true, 'ROLE_ADMIN,ROLE_GENERAL'),
-    ('10000001', 'test2', '$2a$08$7563MWncbW/oenHWfZSKE.jWeMARqdO/WZajbstas2xUamOBXDWz6', 'test2@example.com', 16, true, 'ROLE_GENERAL'),
-    ('10000002', 'test3', '$2a$08$7563MWncbW/oenHWfZSKE.jWeMARqdO/WZajbstas2xUamOBXDWz6', 'test3@example.com', 49, true, 'ROLE_ADMIN');
+DROP TABLE IF EXISTS users;
+-- DROP TABLE IF EXISTS roles_authorities;
+DROP TABLE IF EXISTS roles;
+-- DROP TABLE IF EXISTS authorities;
+
+CREATE TABLE roles(
+	role_id INT AUTO_INCREMENT PRIMARY KEY,
+	name VARCHAR(50) NOT NULL,
+	description VARCHAR(100) NOT NULL,
+	created_at DATETIME NOT NULL,
+	updated_at DATETIME NOT NULL
+);
+
+-- CREATE TABLE authorities(
+-- 	authority_id INT AUTO_INCREMENT PRIMARY KEY,
+-- 	name VARCHAR(50) NOT NULL,
+-- 	description VARCHAR(100) NOT NULL,
+-- 	created_at DATETIME NOT NULL,
+-- 	updated_at DATETIME NOT NULL
+-- );
+
+-- CREATE TABLE roles_authorities(
+-- 	role_id INT NOT NULL,
+-- 	authority_id INT NOT NULL,
+-- 	PRIMARY KEY(role_id, authority_id),
+-- 	FOREIGN KEY(role_id) REFERENCES roles(role_id),
+-- 	FOREIGN KEY(authority_id) REFERENCES authorities(authority_id)
+-- );
+
+create table users (
+	user_id INT AUTO_INCREMENT PRIMARY KEY,
+	user_name VARCHAR(50) NOT NULL,
+	password VARCHAR(128) NOT NULL,
+	email VARCHAR(254) NOT NULL UNIQUE,
+	role_id INT NOT NULL,
+	FOREIGN KEY(role_id) REFERENCES roles(role_id)
+);
+
+set foreign_key_checks = 0;
+TRUNCATE roles;
+set foreign_key_checks = 1;
+
+INSERT INTO roles (name, description, created_at, updated_at)
+		VALUES('general', '一般ユーザ', NOW(), NOW());
+INSERT INTO roles (name, description, created_at, updated_at)
+		VALUES('admin', '管理者ユーザ', NOW(), NOW());
+
+INSERT INTO users(user_name, password, email, role_id) VALUES('test1', '$2a$08$7563MWncbW/oenHWfZSKE.jWeMARqdO/WZajbstas2xUamOBXDWz6', 'test1@example.com', 1);
